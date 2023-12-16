5044a482d0160e7b6424f1c105912bca1625b1c0
Ulaş Sert <ulas.sert@obss.tech>
2023-12-16 17:59:50 +0300

    Command Test

--- app/bot.py
+++ app/bot.py
@@ -1,22 +1,33 @@
 import discord
+from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
-intents.message_content = True
+#intents.message_content = True
 
-client = discord.Client(intents=intents)
+#client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Sync'd {len(synced)} commands.")
+    except Exception as e:
+        print(e)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.tree.command(name='hello')
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hello!")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
 
 bot_token = os.getenv('DISCORD_TOKEN')
-client.run(bot_token)
+bot.run(bot_token)
